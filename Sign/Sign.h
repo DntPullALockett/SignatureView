16cdcf4bf2873f2969971e67ff12c9a63ebf5a0b
Sharad Goyal <sharadgoyal@Sharads-Mac.local>
2018-08-30 12:26:10 +0530

    framework added

---
+++ Sign/Sign.h
@@ -0,0 +1,18 @@
+//
+//  Sign.h
+//  Sign
+//
+//  Created by Sharad Goyal on 20/08/18.
+//  Copyright © 2018 Sharad Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sign.
+FOUNDATION_EXPORT double SignVersionNumber;
+
+//! Project version string for Sign.
+FOUNDATION_EXPORT const unsigned char SignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sign/PublicHeader.h>
+
